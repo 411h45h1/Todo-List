f67a53e339f8085d147a15a79e0e4e6e46d3e12c
Ahmed Ali <ahmedali.96@hotmail.com>
2021-05-14 17:01:52 -0400

    added sass and removed bloat

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="appCont">
+      <header>
+        <h1>
+          TypeScript Todo List
+        </h1>
       </header>
-    </div>
+      <div className="app">
+      <div className='inputCont' ></div>
+      <div className='listCont' ></div>
+
+      </div>
+    </div> 
   );
 }
 
